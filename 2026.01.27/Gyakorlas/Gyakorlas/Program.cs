21fbcadd0a8fd1b4c89c4a55f1e3aee5ba8a741f
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-01-27 12:19:58 +0100

    Add files via upload

---
+++ 2026.01.27/Gyakorlas/Gyakorlas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Armstong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s, t, e;
+            for (int i = 100; i < 999; i++)
+            {
+                s = i / 100;
+                t = i % 10 / 10;
+                e = i % 10;
+                if (s*s*s*t*t*t*e*e*e==i)
+                {
+                    Console.WriteLine(i + "armstrong szám!");
+                }
+                
+            }
+        }
+    }
+}
