20c63d386eb50860cb1c99c64a6756e40a56a056
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-02-04 14:07:33 +0100

    Add files via upload

---
+++ 2026.02.04_/Gyakorlatok2/Lista/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Lista
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string>lista = new List<string>();
+            lista.Add("kvbn");
+            lista.Add("svfonsvi");
+            lista.Add("sdvkjb");
+            lista.Add("sdvksmv");
+
+            lista.Reverse();
+            int index = 1;
+
+            Console.WriteLine("Elements in the list with foreach: ");
+            foreach(string elem in lista)
+            {
+                Console.WriteLine($"A(z) {index} elem a listában: " + elem);
+                index++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Elements in the list with for: ");
+            for (int i=0; i< lista.Count;i++)
+            {
+                Console.WriteLine($"A(z) {i+1} elem a listában: " + lista.ElementAt(i)) ;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Elements in the list with for backwards");
+            for (int i = lista.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"A(z) {i+1} elem listában: " + lista.ElementAt(i));
+            }
+            Console.WriteLine("Elements 2 is present in the list: " + lista.Contains(2));
+
+            lista.Sort();
+            lista.Reverse();
+            index = 0;
+            Console.WriteLine();
+            foreach(int elem  in lista)
+            {
+                Console.WriteLine($"A(z) {index} elem a listában: " + elem);
+
+            }
+        }
+    }
+}
