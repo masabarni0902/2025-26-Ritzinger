3d885d37e8c0e5fa716d7979176a3804e7e45258
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-02-10 11:36:44 +0100

    Add files via upload

---
+++ 2026.02.10/Dolgozat/Feladat1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Feladat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int elso = 0;
+            int masodik = 0;
+            string inoutText = null;
+            Console.WriteLine("Egész számok hányadosának kiszámitása.");
+            Console.WriteLine("Kérem az első egész számot: ");
+            inoutText = Console.ReadLine();
+            //elso = int.Parse(inoutText);
+            elso = Convert.ToInt32(inoutText);
+            Console.Write("Kérem a második egész szzámot: ");
+            inoutText = Console.ReadLine();
+            masodik = Convert.ToInt32(inoutText);
+            Console.WriteLine($"A számok hányados: {(double)elso/masodik:F2} ");
+        }
+    }
+}
---
+++ 2026.02.10/Dolgozat/Feladat2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Feladat2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0,b = 0,c = 0;
+            double x1 =0,x2 =0;
+            string inputText = null;
+            Console.WriteLine("=== Másodfokú egyenlet megoldás ===");
+            Console.WriteLine("Kérem az (a) együthatót: ");
+            inputText = Console.ReadLine();
+            double.TryParse(inputText, out a);
+            Console.Write("Kérem a (b) együtthatót:");
+            double.TryParse(inputText, out b);
+            Console.Write("Kérem a (c) együtthatót:");
+            double.TryParse(inputText, out c);
+            x1 = (-1 * b + Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
+            x2 = (-1 * b - Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
+            Console.WriteLine($"");
+        }
+    }
+}
