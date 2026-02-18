a7559dd0edda5a0ab7341ce65030432804609b09
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-02-18 14:05:16 +0100

    Add files via upload

---
+++ 2026.02.16/Gyakorlatok3/Alagzatok/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Alagzatok
+{
+    public class Circele
+    {
+        private double r = 0;
+        public Circele(double r)
+        {
+            this.r = r;
+        }
+
+        public double Terulet()
+        {
+            return Math.PI * r * r;
+        }
+
+        public double Kerulet()
+        {
+            return Math.PI * 2 * r;
+        }
+        
+        public void Kiir()
+        {
+            Console.WriteLine($"Kőr sugara: {this.r}");
+        }
+
+        /*public override string ToString()
+        {
+
+        }*/
+    }
+
+    public class Rectangle
+    {
+        private double a = 0;
+        private double b = 0;
+
+        public Rectangle(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+         public double Kerulet()
+        {
+            return a * b;
+        }
+
+        public double Terulet()
+        {
+            return 2 * (a + b);
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"Téglalap a: {this.a}, b: {this.b}");
+        }
+
+        /*public override string ToString()
+        {
+
+        }*/
+
+    }
+
+    public class Square
+    {
+        public double a = 0;
+
+        public Square(double a)
+        {
+            this.a = a;
+        }
+
+        public double Terulet()
+        {
+            return a * a;
+        }
+
+        public double Kerulet()
+        {
+            return 4 * a;
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"A Négyzet oldala: {this.a}");
+        }
+
+        /*public override string ToString()
+        {
+
+        }*/
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circele k = new Circele(67.34);
+            k.Kiir();
+            Console.Write("A kör területe:" + k.Terulet());
+            Console.WriteLine();
+            Console.WriteLine("A kör kerülete: " + k.Kerulet());
+            Console.WriteLine(k);
+
+            Square n = new Square(101.34);
+            n.Kiir();
+            Console.WriteLine("A négyzet területe: " + n.Terulet());
+            Console.WriteLine();
+            Console.WriteLine("A négyzet kerülete: " + n.Kerulet());
+            Console.WriteLine(n);
+
+            Rectangle t = new Rectangle(34.12, 56.11);
+            t.Kiir();
+            Console.WriteLine("A tégélalap területe: " + t.Terulet());
+            Console.WriteLine();
+            Console.WriteLine("A téglalap kerülete: " + t.Kerulet());
+            Console.WriteLine(t);
+            
+            
+        }
+    }
+}
---
+++ 2026.02.16/Gyakorlatok3/Dog/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Dog
+{
+    public class Dog
+    {
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public Dog() 
+        {
+            Color = "";
+            Name = "";
+            Breed = "";
+            Height = 0;
+            Width = 0;
+        }
+        public Dog(string color, string name, string breed,double height, double weight)
+        {
+            this.Color = color;
+            this.Name = name;
+            this.Breed = breed;
+            this.Height = height;
+            this.Width = weight;
+        }
+
+        public void WagTail()
+        {
+            Console.WriteLine($"{Name} csoválja a farkát!");
+        }
+
+        public void Shit()
+        {
+            Console.WriteLine($"{Name} kupacot gyárt!");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} ugat: Vau Vau!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} eszik");
+        }
+
+        public void LieDown()
+        {
+            Console.WriteLine($"{Name} lefekszik.");
+        }
+
+        public void Shake()
+        {
+            Console.WriteLine($"{Name} Megrázza magát!");
+        }
+
+        public override string ToString()
+        {
+            return $"Dog: {Name}({Breed}), Color: {Color}, Height: {Height} cm ({Width} kg)";
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog bobby = new Dog();
+            Dog Rex = new Dog("Black","Rex","Német Juhász",50.0,60.0);
+            bobby.Color = "Yellow Brown";
+            bobby.Name = "Bobby";
+            bobby.Breed = "Labrador";
+            bobby.Height = 43.18;
+            bobby.Width = 10.89;
+
+            Console.WriteLine(bobby);
+            bobby.WagTail();
+            bobby.Bark();
+            bobby.Eat();
+            bobby.LieDown();
+            bobby.Shake();
+            bobby.Shit();
+
+            Console.WriteLine(Rex);
+            Rex.WagTail();
+            Rex.Bark();
+            Rex.Eat();
+            Rex.LieDown();
+            Rex.Shake();
+            Rex.Shit();
+        }
+    }
+}
