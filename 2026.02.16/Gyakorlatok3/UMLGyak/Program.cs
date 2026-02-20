2738f75bfdd97e125416eb8b7dcf42c2c3dbd4dc
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-02-20 10:36:48 +0100

    Add files via upload

---
+++ 2026.02.16/Gyakorlatok3/UMLGyak/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace UMLGyak
+{
+    public class Animal
+    {
+        public int age = 0;
+        public string gender;
+
+        public bool isMammal()
+        {
+            return false;
+        }
+
+        public void mate()
+        {
+
+        }
+    }
+
+    public class Duck:Animal
+    {
+        public string beakColor = "red";
+
+        public void swim()
+        {
+
+        }
+
+        public void quack()
+        {
+
+        }
+    }
+
+    public class Fish : Animal
+    {
+        public int sizeInFeet = 0;
+
+        public bool canEat()
+        {
+            return false;
+        }
+    }
+
+    public class Zebra : Animal
+    {
+        public bool is_wild = false;
+
+        public void run()
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
