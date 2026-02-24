c6cbaaf09cba1cde25a748f57cbb3524654bb09e
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-02-24 11:40:46 +0100

    Add files via upload

---
+++ 2026.02.24/UnitTests/nUnitTest1/UnitTest1.cs
@@ -0,0 +1,30 @@
+namespace nUnitTest1
+{
+    public class TestsCsoport1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+
+    public class TestsCsoport2
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
