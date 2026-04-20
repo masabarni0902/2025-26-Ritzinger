fb41e16729021c4e01b8bf7d89b0193a2a356f20
masabarni0902 <55786999+masabarni0902@users.noreply.github.com>
2026-04-20 12:59:59 +0200

    Add files via upload

---
+++ 2026.04.20/WPF2/WpfApp1/3Feladat.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for _3Feladat.xaml
+    /// </summary>
+    public partial class _3Feladat : Window
+    {
+        public _3Feladat()
+        {
+            InitializeComponent();
+        }
+    }
+}
